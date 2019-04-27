e2696b558f85f760b6ec7cae8f2b8b94eb240758
Korotkiy <korotkiy.eugene@gmail.com>
2019-04-27 10:00:12 +0300

    Simplified logic_elements.v

--- lab3/logic_elements.v
+++ lab3/logic_elements.v
@@ -3,24 +3,18 @@ module logic_elements(SW, LEDR);
 input   [9:0]   SW;
 output  [9:0]   LEDR;
 
-wire    [9:0]   y;
-wire    [9:0]   x;
+assign LEDR[0] = ~SW[0];
 
-assign LEDR = y;
-assign x = SW;
+assign LEDR[1] = SW[0] & SW[1];
 
-assign y[0] = ~x[0];
+assign LEDR[2] = ~(SW[0] & SW[1]);
 
-assign y[1] = x[0] & x[1];
+assign LEDR[3] = SW[0] | SW[1];
 
-assign y[2] = ~(x[0] & x[1]);
+assign LEDR[4] = ~(SW[0] | SW[1]);
 
-assign y[3] = x[0] | x[1];
+assign LEDR[5] = SW[0] ^ SW[1];
 
-assign y[4] = ~(x[0] | x[1]);
-
-assign y[5] = x[0] ^ x[1];
-
-assign y[9:6] = 4'b1010;
+assign LEDR[9:6] = 4'b1010;
 
 endmodule

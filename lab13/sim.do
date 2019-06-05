315cdf7991505b88e515697ccb9d8b76c0ce2226
Korotkiy <korotkiy.eugene@gmail.com>
2019-06-05 17:07:48 +0300

    Added design files for lab13 (pwm controller)

--- lab13/sim.do
+++ lab13/sim.do
@@ -8,25 +8,20 @@ if { [file exists "work"] } {
 }
 
 # Create new modelsim working library
-
 vlib work
 
 # Compile all the Verilog sources in current folder into working library
-
-vlog  nco.v nco_tb.v
+vlog  simple_pwm.v simple_pwm_tb.v
 
 # Open testbench module for simulation
-
 vsim -novopt work.testbench
 
 # Add all testbench signals to waveform diagram
-
-add wave /testbench/i_clk
-add wave /testbench/i_rst_n
-add wave /testbench/i_freq_step
-add wave -format Analog-Step -height 84 -max 255.0 -radix unsigned /testbench/o_dac
-add wave /testbench/o_gpio(0)
-add wave /testbench/o_dac_clk
+add wave sim:/testbench/i_clk
+add wave sim:/testbench/i_pwm_en
+add wave sim:/testbench/i_pwm_dc
+add wave sim:/testbench/o_pwm
+add wave sim:/testbench/simple_pwm_inst/cnt_ff
 
 onbreak resume
 
---
+++ lab13/simple_pwm_tb.v
@@ -0,0 +1,42 @@
+`timescale 1ns / 1ps
+
+module testbench;
+
+parameter PERIOD = 20;
+
+wire [35:0] gpio;
+wire        o_pwm;
+reg         i_clk;
+reg         i_pwm_en;
+reg [9:0]   i_pwm_dc;
+
+integer i = 0;
+
+simple_pwm simple_pwm_inst (.CLOCK_50 (i_clk), 
+                            .KEY ({1'b0, i_pwm_en}), 
+                            .SW (i_pwm_dc), 
+                            .GPIO_0 (gpio)
+                           );
+
+assign o_pwm = gpio[0];
+
+initial begin
+    i_clk = 0;
+    forever #(PERIOD/2) i_clk = ~i_clk;
+end
+
+initial begin
+    i_pwm_dc = 0;
+
+    repeat(10) @(negedge i_clk);
+
+    for (i=0; i<1024; i=i+1) begin   
+        i_pwm_dc = i;   
+        i_pwm_en = $random();      
+        repeat (5*1024) @(negedge i_clk);
+    end
+
+    $finish;  
+end
+  
+endmodule
